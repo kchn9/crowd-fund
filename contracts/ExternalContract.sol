d6a60f1dc126a73b496830ab2f251a2a5dd18bdb
kchn9 <mat.kuchnia@gmail.com>
2022-04-07 22:07:11 -0500

    Add Complete event

--- contracts/ExternalContract.sol
+++ contracts/ExternalContract.sol
@@ -5,8 +5,14 @@ pragma solidity ^0.8.0;
  * Example of representation of any external contract that receives stacked funds
  */
 contract ExternalContract {
+    /// @notice Event to keep eye on process completion
+    event Complete(uint collectedAmount);
+
     /// @notice Basic receive funds function
-    function complete() external payable {}
+    /// @dev Emits Complete event 
+    function complete() external payable {
+        emit Complete(msg.value);
+    }
 
     /// @notice Contract balance getter, test purposes only
     function getBalance() public view returns(uint) {
