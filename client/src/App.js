56b7ceec3581ba99f4092d7c75d0e5cd82ddc805
kchn9 <mat.kuchnia@gmail.com>
2022-04-09 19:13:20 -0500

    Refactor getWeb3(), handle connection errors in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,31 +5,39 @@ export const App = () => {
   const [web3, setWeb3] = useState(null);
   const [accounts, setAccounts] = useState(null);
   const [contract, setContract] = useState(null);
+  const [connectionError, setConnectionError] = useState(null);
 
-  useEffect(async () => {
-    (async () => {
+  useEffect(() => {
+    const establishWeb3 = async () => {
       try {
-        // Get network provider and web3 instance.
         const web3 = await getWeb3();
-  
+        setWeb3(web3);
       } catch (error) {
-        // Catch any errors for any of the above operations.
+        console.log("Web3 thrown error during connection");
+        setConnectionError(true);
         console.error(error);
       }
-    })();
+    };
+    establishWeb3();
   }, [])
+
 
   if (!web3) {
     return (
-      <section>
-        Loading Web3, accounts, and contract...
-      </section>
+      <p style={{ textAlign: "center" }}>
+        { connectionError ? 
+          "Error occured - please check your wallet / refresh this site to try again" : 
+          "Loading Web3, accounts, and contract..." 
+        }
+      </p>
     );
-  }
+    }
 
   return (
     <section className="App">
-      <h1>Hello world!</h1>
+      <h1 style={{ textAlign: "center" }}>
+        Connection established!
+      </h1>
     </section>
   )
 }
