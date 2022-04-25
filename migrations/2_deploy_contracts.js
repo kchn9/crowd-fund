38709de3162ece743c02041038efb342c2052cad
kchn9 <mat.kuchnia@gmail.com>
2022-04-25 23:19:29 +0200

    Add .env to deploy file to synchonize testing with deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 var CrowdFund = artifacts.require("./CrowdFund.sol");
+require("dotenv").config({ path: "../.env" });
 
-module.exports = function(deployer) {
-  deployer.deploy(CrowdFund);
+const timeToFundingOver = process.env.TIME_TO_FUNDING_PHASE_OVER; // time in seconds to end stake phase
+const fundingGoal = process.env.FUNDING_GOAL;
+
+module.exports = async function(deployer, _, accounts) {
+  deployer.deploy(CrowdFund, timeToFundingOver, fundingGoal, await accounts[0]);
 };
